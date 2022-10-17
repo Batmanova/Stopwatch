57e7e420585e87880f3bc9ceb6e6c0e4913e46bf
Batmanova <batmanova_s@mail.ru>
2022-10-17 16:52:41 +0300

    first commit

---
+++ Stopwatch/TableRow.swift
@@ -0,0 +1,15 @@
+//
+//  TableRow.swift
+//  Stopwatch
+//
+//  Created by user on 17.10.2022.
+//
+
+import UIKit
+
+class TableRow: UITableViewCell {
+
+    @IBOutlet weak var lapLabel: UILabel!
+    }
+
+
