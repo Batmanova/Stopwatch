a19bd974e923e4bbd44c91d1087f9ffa422cb192
Batmanova <batmanova_s@mail.ru>
2022-10-20 20:14:17 +0300

    first commit

--- Stopwatch/Stopwatch.swift
+++ Stopwatch/Stopwatch.swift
@@ -11,24 +11,69 @@ class Stopwatch: UIViewController, UITableViewDelegate, UITableViewDataSource {
       
     @IBOutlet weak var stopWatchLabel: UILabel!
     @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var lapButton: UIButton!
     
     var stopWatch = Timer()
     var counter: Double = 0.0
     var arrayLap: [String] = []
-    
-    @IBAction func startStopAction(_ sender: UIButton) {
-      stopWatch = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:                       #selector(tikStopWatch), userInfo: nil, repeats: true)
-                }
-    
-    @IBAction func lapAction(_sender:UIButton) {
-        arrayLap.append(stopWatchLabel.text!)
-        table.reloadData()
-        
-    }
-    @objc func tikStopWatch() {
-        counter += 1.0
-        stopWatchLabel.text = String(counter)
+    var playTimer: Bool = true
+    var lapTimer: Bool = true
+           
+     @IBAction func startStopAction(_ sender: UIButton) {
+        if playTimer {
+      stopWatch = Timer.scheduledTimer(timeInterval: 0.035,target: self, selector: #selector(tikStopWatch), userInfo: nil, repeats: true)
+            lapButton.isEnabled = true
+            changeStartStopButton(button: sender, image: "pause.fill", text: "Pause")
+            lapTimer = true
+                   } else {
+                       //отновили тйер
+            stopWatch.invalidate()
+                       //еняе тил нои
+            changeStartStopButton(button: sender, image: "play.fill", text: "Start")
+            lapTimer = false
+            changeLapButton(button: lapButton, image: "trash", text: "Clear")
+         }
         }
+    
+        @IBAction func lapAction(_sender: UIButton) {
+        if lapTimer {
+                arrayLap.append(stopWatchLabel.text!)
+                table.reloadData()
+        }else{
+                guard playTimer else { return }
+                arrayLap.removeAll()
+                table.reloadData()
+                changeLapButton(button: sender, image: "plus", text: "Lap")
+                sender.isEnabled = false
+                stopWatchLabel.text = "00:00"
+                counter = 0.0
+            }
+        }
+    
+        @objc func tikStopWatch() {
+        counter += 0.935
+            var minutes: String = "\((Int)(counter / 60))"
+            if (Int)(counter / 60) < 10 {
+                minutes = "0\((Int)(counter / 60))"
+            }
+            var seconds: String = String(format: "%.1f", counter.truncatingRemainder(dividingBy: 60))
+            if counter.truncatingRemainder(dividingBy: 60) < 10  {
+                seconds = "0" + seconds
+                }
+            stopWatchLabel.text = minutes + ":" + seconds
+    
+        }
+    
+    func changeStartStopButton(button: UIButton, image: String, text: String) {
+       playTimer = !playTimer
+        button.setTitle(text, for: UIControl.State())
+        button.setImage(UIImage(systemName:image), for: UIControl.State())
+    }
+    
+    func changeLapButton(button: UIButton, image: String, text: String) {
+        button.setTitle(text, for: UIControl.State())
+        button.setImage(UIImage(systemName:image), for: UIControl.State())
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return arrayLap.count
@@ -36,11 +81,10 @@ class Stopwatch: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "lapCell", for: indexPath) as! TableRow
+        
         cell.lapLabel.text = arrayLap[indexPath.row]
        
-        
         return cell
     }
-    
-}
+   }
 
